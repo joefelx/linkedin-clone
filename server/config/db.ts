218548137ea932242c6274ecb0a7552441946898
Joe Felix <73597256+joefelx@users.noreply.github.com>
2024-03-15 19:35:46 +0530

    Update db.ts

--- server/config/db.ts
+++ server/config/db.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-  const connect = await mongoose.connect("mongodb+srv://joefelix:mynameisjoefelix2003@joedb.a0ltp9j.mongodb.net/linkedin?retryWrites=true&w=majority");
+  const connect = await mongoose.connect("");
   console.log(`MongoDB connected: ${connect.connection.host}`);
 };
 
